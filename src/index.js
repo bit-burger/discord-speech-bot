95d6096095872a2279d2a9c4c11371fb8619ec9a
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-08-07 13:22:51 +0200

    Added some comments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const { Client } = require("discord.js");
 const { DiscordSR } = require("discord-speech-recognition");
 const ytdl = require("ytdl-core");
 
+// DiscordSR adds the text-to-speech functionality
 const client = new Client();
 new DiscordSR(client);
 
+// loadConfig.js loads and validates the speech-config.json
 const { token, speech, logging } = require("./loadConfig");
 
+// Goes in channel if at least one real bot person is in there.
+// Only leaves the channel if no real person is left
 client.on("voiceStateUpdate", async (oldState, newState) => {
   if (oldState.member.bot) return;
   if (oldState.channelID === newState.channelID) return;
@@ -49,6 +53,11 @@ function sleep(milliseconds) {
 client.on("speech", (message) => {
   if (message.content === "all") {
     console.log(message ?? "[EMPTY BUFFER]");
+// Will check each child of 'speech' for its speechData.
+// The speechData contains an array of arrays with words in it.
+// If each word of one of the subarrays is found in the transcript,
+// the first valid child's playResource is then taken and played.
+// Will also check the logging configuration to log at the correct time
   }
   if (!message.content) return;
   if (message.content === "allWithoutEmpty") {
--- src/loadConfig.js
+++ src/loadConfig.js
@@ -3,9 +3,11 @@ const config = require("../speech-config.json");
 const Ajv = require("ajv").default;
 const ajv = new Ajv({ allErrors: true, useDefaults: true });
 
+// Makes custom error messages in the schema possible
 require("ajv-errors")(ajv);
 const validate = ajv.compile(schema);
 
+// Prints error and exists the program, if the validation does not work
 if (!validate(config)) {
   console.error(
     "There are some errors in your speech-config.json:\n\n" +
